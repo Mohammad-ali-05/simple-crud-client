da454bd161b7a9e3127fd3481bf48bdf6c8c7e0f
Mohammad Ali <muhammadali.pass1@gmail.com>
2026-02-10 10:47:43 +0600

    Added delete button and a function to send a delete request to the server. After successfully deleting the user a success toast will be shown as well as updeating the UI without reloading the page.

--- src/Components/Users.jsx
+++ src/Components/Users.jsx
@@ -34,15 +34,37 @@ const Users = () => {
                 console.log(`Data after save: `, data);
                 if (data.insertedId) {
                     e.target.reset();
-                    newUser._id = data.insertedId
-                    setUsers([...users, newUser])
+                    newUser._id = data.insertedId;
+                    setUsers([...users, newUser]);
                     alert("User added successfully");
+                }
+            });
+    };
+
+    const handleDeleteUser = (userId) => {
+        const id = { userId };
+
+        fetch("http://localhost:3000/users", {
+            method: "DELETE",
+            headers: {
+                "content-type": "application/json",
+            },
+            body: JSON.stringify(id),
+        })
+            .then((res) => res.json())
+            .then((data) => {
+                console.log(data);
+                if (data.deletedCount) {
+                    alert("User deleted successfully")
+                    const newUsers = users.filter(user => user._id !== userId)
+                    setUsers(newUsers)
                 }
             });
     };
 
     return (
         <div>
+            <p>Users: {users.length}</p>
             <form onSubmit={handleAddUser}>
                 <input type="text" name="name" />
                 <br />
@@ -54,7 +76,11 @@ const Users = () => {
             <div>
                 {users?.map((user) => (
                     <p key={user._id}>
-                        {user.name} : {user.email}
+                        {" "}
+                        {user.name} : {user.email}{" "}
+                        <button onClick={() => handleDeleteUser(user._id)}>
+                            x
+                        </button>
                     </p>
                 ))}
             </div>
